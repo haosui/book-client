2b7d77bbe3f506c75d188cb7b59bcbe1ed1bdad9
Uaena1711 <haipn72@gmail.com>
2020-09-21 21:27:12 +0700

    fix router link and add home component

--- angular-Book-Store/src/app/app-routing.module.ts
+++ angular-Book-Store/src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { BookComponent } from './book/book.component';
+
+import { HomeComponent } from './home/home.component';
 
 const routes: Routes = [
+  {path: 'home', component: HomeComponent},
+  {path: 'book', component: BookComponent},
+  {path: '', redirectTo: '/home', pathMatch: 'full'}
 ];
 
 @NgModule({
--- angular-Book-Store/src/app/app.module.ts
+++ angular-Book-Store/src/app/app.module.ts
@@ -2,13 +2,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { browser } from 'protractor';
-// import { BookComponent } from './book/book.component';
+import { BookComponent } from './book/book.component';
+import { HomeComponent } from './home/home.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
+    BookComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
