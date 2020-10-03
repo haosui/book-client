e580b30170e042f2d6f5209ee3053b89a3351e61
=HOANGTHIYEN <=hoangyen2999@gmail.com>
2020-10-03 10:29:09 +0700

    update aboutUs

--- angular-Book-Store/src/app/app-routing.module.ts
+++ angular-Book-Store/src/app/app-routing.module.ts
@@ -6,12 +6,14 @@ import { HomeComponent } from './home/home.component';
 import { CartComponent } from './cart/cart.component';
 import { CheckoutComponent } from './checkout/checkout.component';
 import { ShopComponent } from './shop/shop.component';
+import { AboutUsComponent } from './about-us/about-us.component';
 const routes: Routes = [
   {path: 'home', component: HomeComponent},
   {path: 'book', component: BookComponent},
   {path: 'cart', component: CartComponent},
   {path: 'checkout', component: CheckoutComponent },
   {path: 'shop', component: ShopComponent},
+  {path: 'about-us', component: AboutUsComponent},
   {path: '', redirectTo: '/home', pathMatch: 'full'}
 ];
 
