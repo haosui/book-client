e6e3917fa8486f3bfd5d03f0536b45bf499d20d9
Nguyen Tuan Anh <55202180+anhtuanng689@users.noreply.github.com>
2020-10-12 01:21:06 +0700

    add basic book-detail

--- angular-Book-Store/src/app/book/book.component.ts
+++ angular-Book-Store/src/app/book/book.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit , Output, EventEmitter, Inject, Injectable} from '@angular/core';
-
+import { HighBookList, NewBookList, ReBookList, ChildrentBookList, EnglishBookList, NovelBookList } from '../book/Book-Mock';
+import { NzRateModule } from 'ng-zorro-antd/rate';
+import { NzInputNumberModule } from 'ng-zorro-antd/input-number';
 
 @Component({
   selector: 'app-book',
@@ -8,11 +10,16 @@ import { Component, OnInit , Output, EventEmitter, Inject, Injectable} from '@an
 })
 @Injectable()
 export class BookComponent implements OnInit {
-
-
+  hbooks = HighBookList;
+  nbooks = NewBookList;
+  rbooks = ReBookList;
+  cbooks = ChildrentBookList;
+  ebooks = EnglishBookList;
+  novbooks = NovelBookList;
+  bookQuantity = 1;
 
   constructor() {
-   }
+  }
 
   ngOnInit(): void {
   }
