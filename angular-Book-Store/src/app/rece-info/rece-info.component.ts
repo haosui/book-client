3a3f1993a128a483f548eedef4fbeab99124f2c6
lightning dark <pvdung0310@gmail.com>
2020-10-14 21:44:54 +0700

    update fixloi loi

--- angular-Book-Store/src/app/rece-info/rece-info.component.ts
+++ angular-Book-Store/src/app/rece-info/rece-info.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { PrimeNGConfig} from 'primeng/api';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
+import { CusInfo } from '../rece-info/customerinfo';
+
 @Component({
   selector: 'app-rece-info',
   templateUrl: './rece-info.component.html',
@@ -13,39 +15,39 @@ export class ReceInfoComponent implements OnInit {
   
   infodisplay:boolean = !this.formdisplayy;
   submitted = false;
-  thongtin = {
+  thongtin: CusInfo ={
     ten: "",
     sdt: "",
     diachi: ""
   };
 
-  strinbuton: string = " Sửa";
+  userinfos: Array<CusInfo> =[];
  
   tinh = [
-  { id: '0', name: 'tinh 1' },
-  { id: '1', name: 'thinh 2' },
+  { id: '0', name: 'Hà Nội' },
+  { id: '1', name: 'Hải Dương' },
   
   ];
 huyen1 = [
-  { id: '0', name: 'quan 1 cua tinh 1' },
-  { id: '1', name: 'thinh 2 cua tinh 1' },
+  { id: '0', name: 'cầu Giấy' },
+  { id: '1', name: 'Long Biên' },
   
-  { id: '2', name: 'quan 1 cua tinh 2' },
-  { id: '3', name: 'thinh 2 cua tinh 2' },
+  { id: '2', name: 'tp Hải Dương' },
+  { id: '3', name: 'Thanh Hà' },
 ];
 xa1 = [
-  { id: '0', name: ' xa 1 quan 1 cua tinh 1' },
-  { id: '1', name: 'xa 2 quan 1 cua tinh 1' },
+  { id: '0', name: 'Xuân thủy' },
+  { id: '1', name: 'Hồ Tùng Mậu' },
 
 
-  { id: '2', name: ' xa 1 quan 2 cua tinh 1' },
-  { id: '3', name: 'xa 2 quan 2 cua tinh 1' },
+  { id: '2', name: 'Hoàng Như Tiếp' },
+  { id: '3', name: 'Gia Lâm' },
 
-  { id: '4', name: ' xa 1 quan 1 cua tinh 2' },
-  { id: '5', name: 'xa 2 quan 1 cua tinh 2' },
+  { id: '4', name: 'Ngô Quyền' },
+  { id: '5', name: 'Trần Hưng Đạo' },
 
-  { id: '6', name: ' xa 1 quan 2 cua tinh 2' },
-  { id: '7', name: 'xa 2 quan 2 cua tinh 2' },
+  { id: '6', name: 'xã gì đó của Thanh Hà 1' },
+  { id: '7', name:  'xã gì đó của Thanh Hà  2' },
 ];
 huyen = [
   
@@ -58,16 +60,7 @@ xa = [
   
   
   ngOnInit(){
-    this.loadInfo();
-    var splitted = this.thongtin.diachi.split(",", 3);
-    console.log(splitted);
-
-    var tinhid = this.tinh.find(f => f.name === splitted[2]);
-    var huyenid = this.huyen1.find(f => f.name === splitted[1]);
-    var xaid = this.xa1.find(f => f.name === splitted[0]);
-    this.huyen = [...this.huyen1].splice(parseInt(tinhid.id)*2,2);
-    this.xa = [...this.xa1].splice(parseInt(huyenid.id)*2,2);
-
+    
 
     this.registerForm = this.formBuilder.group({
       
@@ -75,9 +68,9 @@ xa = [
       hoten: [this.thongtin.ten, Validators.required],
       dienthoai: [this.thongtin.sdt, [Validators.required,Validators.pattern("^[0-9]*$"), Validators.minLength(8)]],
 
-      tinh: [tinhid.id, Validators.required],
-      huyen: [huyenid.id, Validators.required],
-      xa: [xaid.id, Validators.required],
+      tinh: ['', Validators.required],
+      huyen: ['', Validators.required],
+      xa: ['', Validators.required],
 
     
 
@@ -123,13 +116,15 @@ saveInfo()
     console.log(this.registerForm.value);
     this.thongtin.diachi = this.getxabyid(this.registerForm.value.xa) +","+this.gethuyenbyid( this.registerForm.value.huyen)+","+ this.gettinhbyid(this.registerForm.value.tinh);
     console.log(this.thongtin);
-    localStorage.setItem('infoUser',JSON.stringify(this.thongtin));
+    let Ob: Array<CusInfo> = JSON.parse(localStorage.getItem('thongtinkhach'));
+    if(Ob)
+    {
+      this.userinfos = Ob;
+    }
+    this.userinfos.push(this.thongtin);
+    localStorage.setItem('thongtinkhach',JSON.stringify(this.userinfos));
+}
 
-}
-loadInfo()
-{
-  this.thongtin = JSON.parse( localStorage.getItem('infoUser'));
-}
  gettinhbyid(id)
  {
     return this.tinh[parseInt(id)].name;
@@ -152,29 +147,23 @@ xoaform()
 }
 onEditt()
 {
-  if(this.formdisplayy)
-  {
+  
     
     this.submitted = true;
     if (this.registerForm.invalid) {
+      
         return;
     }
     else{
-      this.strinbuton = 'Sửa';
+      
     this.saveInfo();
-    this.formdisplayy = !this.formdisplayy;
+    this.router.navigate(['checkout']);
+    console.log('loacl',localStorage.getItem('thongtinkhach'));
     }
     
-  }
-  else{
-    this.loadInfo(); 
-    this.strinbuton = 'Lưu';
-    this.formdisplayy = !this.formdisplayy;
-
-   
-   
-  }
-  
-  
 }
+  
+  
+  
+
 }
